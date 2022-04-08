b5ebd984fe9e169c578401d45c22a6217a83d552
esp3j0 <aesp3j0@gmail.com>
2022-04-08 15:15:37 +0800

    4.8-15

--- server.go
+++ server.go
@@ -6,10 +6,9 @@ import (
 )
 
 type Server struct {
-	ip             string
-	port           int
-	Broadcast_chan chan string
-	OlineMap       map[string]*User
+	ip       string
+	port     int
+	OlineMap map[string]*User
 }
 
 func (t *Server) Start() {
@@ -18,8 +17,6 @@ func (t *Server) Start() {
 		fmt.Printf("listen err: %v\n", err)
 	}
 	defer listener.Close()
-	//开启广播机制
-	go t.Broadcast()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -34,29 +31,37 @@ func (t *Server) Start() {
 //用户业务逻辑
 func (t *Server) Handler(conn net.Conn) {
 	//创建用户
-	user := NewUser(conn, t.Broadcast_chan)
-	//将用加入全局用户表中
-	t.OlineMap[user.Conn.RemoteAddr().String()] = user
+	user := NewUser(conn, t)
 	//监听user维护的chan
 	go user.listenC()
 	go user.Reader()
 }
 
-func (t *Server) Broadcast() {
-	for {
-		mes := <-t.Broadcast_chan
+func command(mes string) bool {
+	for _, i := range []string{"list"} {
+		if i == mes {
+			return true
+		}
+	}
+	return false
+}
+
+func (t *Server) Broadcast(user *User, mes string) {
+	if command(mes) {
+	} else {
 		for _, one_user := range t.OlineMap {
-			one_user.C <- mes
+			if one_user != user {
+				one_user.C <- user.Name + ": " + mes
+			}
 		}
 	}
 }
 
 func NewServer(ip string, port int) (server *Server) {
 	server = &Server{
-		ip:             ip,
-		port:           port,
-		OlineMap:       make(map[string]*User),
-		Broadcast_chan: make(chan string),
+		ip:       ip,
+		port:     port,
+		OlineMap: make(map[string]*User),
 	}
 	return
 }
--- user.go
+++ user.go
@@ -6,10 +6,10 @@ import (
 )
 
 type User struct {
-	Name          string
-	Conn          net.Conn
-	C             chan string
-	broadcastchan chan string
+	Name   string
+	Conn   net.Conn
+	C      chan string
+	Server *Server
 }
 
 //监听C管道,将C管道中内容发送给对应客户端
@@ -26,21 +26,43 @@ func (t *User) Reader() {
 	for {
 		n, err := t.Conn.Read(buf)
 		if err != nil || n == 0 {
-			fmt.Printf(t.Name + "退出\n")
-			t.broadcastchan <- string(t.Name + "退出\n")
+			t.Exit()
 			return
 		}
-		t.broadcastchan <- t.Name + ": " + string(buf[:n-1])
+		t.Server.Broadcast(t, string(buf[:n-1]))
+		// else if string(buf[:n-1]) == "list" {
+		// 	for key := range t.Server.OlineMap {
+		// 		t.Conn.Write([]byte(key + "\n"))
+		// 	}
+
+		// } else {
+
+		// }
+
 	}
 }
 
-func NewUser(conn net.Conn, broadcastchan chan string) (user *User) {
+//用户上线
+func (t *User) Login() {
+	//将用加入全局用户表中
+	t.Server.OlineMap[t.Conn.RemoteAddr().String()] = t
+	t.Server.Broadcast(t, t.Name+"上线")
+}
+
+//用户下线
+func (t *User) Exit() {
+	delete(t.Server.OlineMap, t.Conn.RemoteAddr().String())
+	fmt.Println(t.Name + "连接断开")
+	t.Server.Broadcast(t, t.Name+"下线")
+}
+
+func NewUser(conn net.Conn, server *Server) (user *User) {
 	user = &User{
-		Name:          conn.RemoteAddr().String(),
-		Conn:          conn,
-		C:             make(chan string),
-		broadcastchan: broadcastchan,
+		Name:   conn.RemoteAddr().String(),
+		Conn:   conn,
+		C:      make(chan string),
+		Server: server,
 	}
-	broadcastchan <- user.Name + "已连接"
+	user.Login()
 	return
 }
